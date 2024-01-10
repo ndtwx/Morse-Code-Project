9920a6ae3433298bca0cc278cfb084f6c4d9f9c2
ndtwx <nd_twx@hotmail.com>
2024-01-11 00:39:49 +0800

    added option to exit the program

--- main.py
+++ main.py
@@ -18,40 +18,45 @@ morse_code_list = [value for (key, value) in morse_dict.items()]
 # print(morse_dict_swap)
 # print(list_of_morse_signs)
 def converter(option):
-    if option == "1":
+
+    if option == "exit":
+        exit()
+
+    elif option == "1":
+        converted_text = ""
         text = input("\nType your text here: ").upper()
 
-        converted_text = ""
+        if text == "":
+            print("There's nothing to convert. Please try again\n")
 
-        for char in text:
-            converted_text += morse_dict[char] + ' '
-
-        print(f"Converted text: {converted_text}\n")
+        else:
+            for char in text:
+                converted_text += morse_dict[char] + ' '
+            print(f"Converted text: {converted_text}\n")
 
     elif option == "2":
         code = input("\nType your code here: ").split(" ")
-
         converted_code = ""
 
         for char in code:
             if char not in morse_code_list:
-                print("\nThere are no such codes, please enter again.\n")
+                print("There are no such codes, please try again.\n")
                 start()
-
             else:
                 converted_code += morse_dict_swap[char]
-
         print(f"Converted code: {converted_code}\n")
 
     else:
-        print("\nYou have chosen the wrong option, Please choose one of the available options\n")
+        print("You have chosen the wrong option, Please choose one of the available options\n")
 
-def start():
-    should_continue = True
+
+def start(should_continue=True):
     while should_continue:
+        option = " "
         print("|TEXT/MORSE CODE CONVERTER|")
         print("\n1. Enter 1 to convert text to code")
         print("2. Enter 2 to convert code to text")
+        print("3. Enter exit to exit the program\n")
 
         option = input("Enter your option: ").lower()
 
